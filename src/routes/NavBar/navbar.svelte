60b0b9f33442b504b069e05fcc69d890842a5480
Shivam Newase <shivam.newase@inflectionzone.com>
2022-06-29 15:37:47 +0530

    cards,Icons and sidebar are updated

--- src/routes/NavBar/navbar.svelte
+++ src/routes/NavBar/navbar.svelte
@@ -1,31 +1,34 @@
 <script>
 
+import Fa from 'svelte-fa'
+  import { faCog,faBell,faHome } from '@fortawesome/free-solid-svg-icons'
+
+   
 </script>
 
-<div class="navbar bg-[#7165e3]">
+
+<div class="navbar bg-[#7165e3] ">
     <div class="flex-1">
      
         <button class="btn btn-square btn-ghost">
+          <label for="my-drawer-2" class="btn btn-primary ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </label>
           </button>
 
     </div>
+   
+   
     <div class="flex-none">
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle">
           <div class="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-            <span class="badge badge-sm indicator-item">8</span>
+              <Fa icon={faHome} size="lg"/>
           </div>
         </label>
         <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
           <div class="card-body">
-            <span class="font-bold text-lg">8 Items</span>
-            <span class="text-info">Subtotal: $999</span>
-            <div class="card-actions">
-              <button class="btn btn-primary btn-block">View cart</button>
-            </div>
-            
+            <!-- card content dropdown menu of card -->
           </div>
         </div>
       </div>
@@ -33,14 +36,22 @@
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle">
           <div class="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
+             <Fa icon={faBell} size="lg"/>
           </div>
         </label>
        
       </div>
-
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <div class="indicator">  
+                <Fa icon={faCog} size="lg" />
+          </div>
+        </label>
+      </div>
+      
+    
      
+  
        
      
 
@@ -68,4 +79,5 @@
         </ul>
       </div> -->
     </div>
+
   </div>
