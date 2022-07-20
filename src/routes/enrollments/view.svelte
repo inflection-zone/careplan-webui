481bd243662f62a9a38bcb4a998f3052ac0dc742
Shivam Newase <shivam.newase@inflectionzone.com>
2022-07-20 17:06:01 +0530

    index page for careplan and enrollment

---
+++ src/routes/enrollments/view.svelte
@@ -0,0 +1,93 @@
+<script>
+</script>
+
+<div class=" breadcrumbs text-lg mt-4 text-[#7165E3] ml-14 mt-10">
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li />
+	</ul>
+</div>
+
+<div class="flex flex-row mx-14 mt-4 ">
+	<div class="basis-1/2 justify-center items-center ">
+		<div class="relative flex items-center  ">
+			<h3 class="font-semibold text-2xl">Enrollments</h3>
+		</div>
+	</div>
+</div>
+
+<div class=" flex justify-center items-center mx-14 mt-12 ">
+	<div class="card w-full h-full  shadow-xl">
+		<div class="flex items-center mb-4 mt-10 mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">Participant Age</label>
+			</div>
+			<div class="w-2/3">44 years 3 months</div>
+		</div>
+		<div class="flex items-center mb-4  mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">Participant Gender</label>
+			</div>
+			<div class="w-2/3">Female</div>
+		</div>
+		<div class="flex items-center mb-4  mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">CarePlan</label>
+			</div>
+			<div class="w-2/3">Maternity</div>
+		</div>
+		<div class="flex items-center mb-4  mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">CarePlan Category</label>
+			</div>
+			<div class="w-2/3">Maternity and Neo Natal</div>
+		</div>
+		<div class="flex items-center mb-4  mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">Date of Enrollment</label>
+			</div>
+			<div class="w-2/3">July 20,2022</div>
+		</div>
+		<div class="flex items-center mb-4  mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">Start Date</label>
+			</div>
+			<div class="w-2/3">July 20,2022</div>
+		</div>
+		<div class="flex items-center mb-4 mx-16">
+			<div class="w-1/6">
+				<label class="lable-text">End Date</label>
+			</div>
+			<div class="w-2/3">Oct 30,2022</div>
+		</div>
+    
+        <div class="flex flex-row justify-between my-10 mx-24">
+            <div class="grid grid-cols2">
+                <label class="lable-text pl-2 text-xl">End Date</label>
+                <div class="radial-progress" style="--value:100;">100%</div>
+            </div>
+            <div class="grid grid-cols2">
+                <label class="lable-text pl-2">End Date</label>
+                <div class="radial-progress" style="--value:100;">100%</div>
+            </div>
+            <div class="grid grid-cols2">
+                <label class="lable-text pl-2">End Date</label>
+                <div class="radial-progress" style="--value:100;">100%</div>
+            </div>
+            <div class="grid grid-cols2">
+                <label class="lable-text pl-2">End Date</label>
+                <div class="radial-progress" style="--value:100;">100%</div>
+            </div>
+         
+        </div>
+       
+
+
+	</div>
+
+   
+
+
+
+
+</div>
